99a4b6d1c18586c29a3b303a11f859420a43847f
Alfred Lin <alfredl@email.sc.edu>
2022-11-20 13:20:03 -0500

    srcLogin added

---
+++ client/srcLogin/components/loginform.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./loginform.css"
+
+const LoginForm = () => {
+
+    return (
+        <div className="cover">
+            <h1>
+                Login
+            </h1>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <div className="login-btn">Login</div>
+            <p className="text">
+                Or create an account
+                <a href='# ' > here  
+                </a>
+            </p>
+        </div>
+    )
+}
+
+export default LoginForm
