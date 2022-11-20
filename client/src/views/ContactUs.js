cc1d45285f901b548ec27c89d645d0476974987b
Jack <jackcoberman@gmail.com>
2022-11-20 13:55:15 -0500

    fixed html issue with contact form

--- client/src/views/ContactUs.js
+++ client/src/views/ContactUs.js
@@ -3,44 +3,55 @@
 // with help from tutorial at https://www.youtube.com/watch?v=bMq2riFCF90
 
 import React, { useRef } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-    const form = useRef();
-    
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const form = useRef();
 
-        emailjs.sendForm('service_ddw3f6r', 'template_peypdcl', form.current, 'MEck6kXn4BPEa6daF')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    }
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    return <>
+    emailjs
+      .sendForm(
+        "service_ddw3f6r",
+        "template_peypdcl",
+        form.current,
+        "MEck6kXn4BPEa6daF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Contact Us</h1>
+        <p>Send an email to the Bookstore by filling out the form below.</p>
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
         <div>
-            <h1>Contact Us</h1>
-            <text>Send an email to the Bookstore by filling out the form below.</text>
+          <label>Name: </label>
+          <input type="text" name="from_name" />
         </div>
-        <form ref={form} onSubmit={sendEmail}>
-            <div>
-                <label>Name: </label>
-                <input type="text" name="from_name" />
-            </div>
-            <div>
-                <label>Email: </label>
-                <input type="email" name="from_email" />
-            </div>
-            <text>{" "}</text>
-            <div>
-                <label>Message: </label>
-                <textarea name="message" />
-            </div>
-            <input type="submit" value="Send" />
-        </form>
+        <div>
+          <label>Email: </label>
+          <input type="email" name="from_email" />
+        </div>
+        <p> </p>
+        <div>
+          <label>Message: </label>
+          <textarea name="message" />
+        </div>
+        <input type="submit" value="Send" />
+      </form>
     </>
+  );
 };
 
 export default ContactForm;
