07a83e801fe4e17a191471b3d0cfc6ce5764c241
Rahul Bulusu <rbulusu@email.sc.edu>
2022-11-29 10:42:01 -0500

    finished the home page

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -4,8 +4,18 @@ function Home() {
   return (
     <div className="home">
       <h1 style={{ color: 'green' }}>Welcome</h1>
-      <p>
+      <h2>
         Welcome to the Book Store Website!
+        If You Love Books, You'll Love Our Book Store!
+      </h2>
+    
+      <h2>November 27 - Weekly Newsletter</h2>
+      <p>
+      New Books will be coming in the next week. We will also be having a signing day for a very special author. STAY TUNED FOR MORE INFO!!
+      </p>
+      <h2>November 21 - The Biggest Sale Ever Now Through New Years Day </h2>
+      <p>
+       40%, 60%, and 80% off on best sellers. Come in store today and get an additional 20% on your total purchase.
       </p>
     </div>
   );
