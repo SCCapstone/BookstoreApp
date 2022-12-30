499adf2a03d712959beb048959c8b8d96492658e
Jack <jackcoberman@gmail.com>
2022-12-29 21:28:51 -0500

    new lines at the end of files

--- client/src/views/Browse.js
+++ client/src/views/Browse.js
@@ -1,63 +1,64 @@
 // help from tutorial at https://www.youtube.com/watch?v=x7niho285qs
 
-import React, { useState } from 'react';
-import "./Searchbar.css"
-import { BiSearchAlt2 } from "react-icons/bi"
-import { AiFillCloseSquare } from "react-icons/ai"
+import React, { useState } from "react";
+import "./Searchbar.css";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { AiFillCloseSquare } from "react-icons/ai";
 
-function Browse({placeholder, data}) {
-    const [filteredData, setFilteredData] = useState([]);
-    const [wordEntered, setWordEntered] = useState("");
+function Browse({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
 
-    const handleFilter = (event) => {
-        const searchWord = event.target.value
-        setWordEntered(searchWord);
-        const newFilter = data.filter((value) => {
-            return value.title.toLowerCase().includes(searchWord.toLowerCase());
-        });
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
 
-        if(searchWord === "") {
-            setFilteredData([])
-        } else {
-            setFilteredData(newFilter);
-        }
-    };
-    
-    const clearInput = () => {
+    if (searchWord === "") {
       setFilteredData([]);
-      setWordEntered("");
-    };
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
 
-    return (
-        <div className='browse'>
-            <h1 style={{ color: 'green' }}>Browse</h1>
-            <div className="searchInputs">
-                <input type="text" 
-                    placeholder={placeholder} 
-                    value={wordEntered} 
-                    onChange={handleFilter} 
-                />
-                <div className='searchIcon'>
-                    {filteredData.length === 0 ? (
-                        <BiSearchAlt2 /> 
-                    ) : (
-                        <AiFillCloseSquare id="clearBtn" onClick={clearInput} />
-                    )}
-                </div>
-            </div>
-            {filteredData.length !== 0 && (
-                <div className="dataResult">
-                    {filteredData.slice(0, 15).map((value, key) => {
-                        return (
-                            <a className='dataItem' href={value.imageLink} > 
-                                <p>{value.title} </p>
-                            </a>
-                        );
-                    })}
-                </div>
-            )}
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className="browse">
+      <h1 style={{ color: "green" }}>Browse</h1>
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">
+          {filteredData.length === 0 ? (
+            <BiSearchAlt2 />
+          ) : (
+            <AiFillCloseSquare id="clearBtn" onClick={clearInput} />
+          )}
         </div>
-    );
+      </div>
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a className="dataItem" href={value.imageLink}>
+                <p>{value.title} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Browse;
