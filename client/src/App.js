7fc9fe72e74e61c8e0e7ec4444552ade0b5e9f85
Jack <jackcoberman@gmail.com>
2022-12-30 11:33:46 -0500

    Fixed margins on background

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import ContactForm from "./views/ContactUs";
@@ -8,32 +9,28 @@ import NavBars from "./components/NavBars/NavBar";
 import TermsServices from "./views/TermsServices";
 import LoginForm from "./views/Login/loginform";
 import AdminPage from "./views/AdminPage/AdminPage";
-//import "./App.css";
 
 var React = require("react");
 var Component = React.Component;
 
 class App extends Component {
   render() {
-    //const { data } = this.state;
     const user = localStorage.getItem("token");
     return (
-      <div style={{ background: "#B49A6773", height: "99vh", width: "99vw" }}>
-        <div className="App">
-          <BrowserRouter>
-            <NavBars user={user} />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/browse" element={<Browse />} />
-              <Route path="/contactus" element={<ContactForm />} />
-              <Route path="/about" element={<AboutUs />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/termsservices" element={<TermsServices />} />
-              <Route path="/login" element={<LoginForm />} />
-              <Route path="/users" element={<AdminPage user={user} />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
+      <div className="App">
+        <BrowserRouter>
+          <NavBars user={user} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/browse" element={<Browse />} />
+            <Route path="/contactus" element={<ContactForm />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/termsservices" element={<TermsServices />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/users" element={<AdminPage user={user} />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
