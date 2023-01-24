248afd67fb5221e2465e35cda8110b8b52670bff
s-orug <orugants@email.sc.edu>
2023-01-24 18:43:56 -0500

    update

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -32,40 +50,50 @@ const Login = () => {
       }
     }
   };
-  
+
   return (
     <div class="pb-4 h-full text-gray-800 max-w-[1400px]">
       <div class="flex xl:justify-center lg:justify-between justify-center items-center grid grid-cols-3 h-full g-6">
         <div />
         <div class="w-full py-16 gap-2 ">
-            <div class="pb-4">
-              <div class="grid bg-polished_pine text-center text-white border-black border-2 text-3xl rounded py-3 ">
+          <div class="pb-4">
+            <div class="grid bg-polished_pine text-center text-white border-black border-2 text-3xl rounded py-3 ">
               Login
             </div>
           </div>
-          <form class="justify-center">
-            <div class="mb-6">
-              <TextField
+          <form class="justify-center space-y-4">
+            {/* <TextField
                 name="email"
                 value={data.email}
-                class="form-control placeholder-black block w-full px-4 py-2 text-xl font-normal text-black bg-camel border border-solid rounded focus:bg-white"
-                label="Enter Email"
+                className="form-control placeholder-black block w-full px-4 py-4 text-xl font-normal text-black bg-camel border border-solid rounded focus:bg-white"
+                placeholder="Enter Email"
                 onChange={handleChange}
+                error={false}
+                helperText=""
               />
-            </div>
 
-            <div class="mb-6">
-              <input
+              <TextField
                 type="password"
                 name="password"
                 value={data.password}
-                class="form-control placeholder-black block w-full px-4 py-2 text-xl font-normal text-black bg-camel border border-solid border-black rounded focus:bg-white"
+                className="form-control text-black placeholder-black block w-full px-4 py-4 text-xl font-normal text-black bg-camel border border-solid rounded focus:bg-white"
                 placeholder="Enter Password"
+                error={false}
                 onChange={handleChange}
-              />
-            </div>
+                helperText=""
+              /> */}
 
-
+            <TextField label="Enter Email" color="primary" />
+            <TextField
+              type="password"
+              name="password"
+              value={data.password}
+              label="Enter Password"
+              error={false}
+              onChange={handleChange}
+              helperText=""
+              className="max-w-[50%]"
+            />
 
             <div class="text-center lg:text-left grid pb-6">
               <Button
