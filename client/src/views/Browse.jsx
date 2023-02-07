6ef8f764e58d6a991b4777de9b4a7814c47100c7
s-orug <orugants@email.sc.edu>
2023-02-07 17:33:03 -0500

    update

--- client/src/views/Browse.jsx
+++ client/src/views/Browse.jsx
@@ -3,11 +3,10 @@ import { styles } from "../styles";
 import books from "./Books";
 
 import * as React from "react";
-import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 
 const sort = (books, value) => {
   console.log(value);
