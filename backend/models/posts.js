d7654ba6715f3b790c96e346f357983f72f1736f
William Hobbs <13199119+wihobbs@users.noreply.github.com>
2023-02-07 12:49:33 -0500

    backend for posting

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -5,7 +5,10 @@ const postSchema = new mongoose.Schema({
     uuid: { type: String, required: true },
     bookId: { type: String, required: true },
     post: { type: String, required: true },
+    replies: { type: JSON, required: false },
 });
+
+// replies will be a json object with name and reply so people can reply
 
 const Post = mongoose.model("post", postSchema);
 
