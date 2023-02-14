9d4356f12517eb2ff1f9c3d54a7c6e2b2511cb96
s-orug <orugants@email.sc.edu>
2023-02-14 17:05:50 -0500

    profile drop down works

--- client/src/components/ProfileIconDropdown.jsx
+++ client/src/components/ProfileIconDropdown.jsx
@@ -2,15 +2,17 @@ import {React, useState} from "react";
 import {FaUser} from "react-icons/fa";
 
 const ProfileIconDropdown = () => {
-  const [state, setState] = useState(0);
+  const states = ["hidden", ""]
+  const [state, setState] = useState("");
+  const counter = 0;
   return(
   <div>
       <button id="dropdownAvatarNameButton" data-dropdown-toggle="dropdownAvatarName" class="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:mr-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-white" type="button">
           <span class="sr-only">Open user menu</span>
-          <FaUser className="w-8 h-8 mr-2 rounded-full" alt="user photo"/>
+          <FaUser className="w-8 h-8 mr-2 rounded-full" alt="user photo" onClick={() => {setState(states[counter%2]); counter++}}/>
           <svg class="w-4 h-4 mx-1.5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
-      <div id="dropdownAvatarName" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+      <div id="dropdownAvatarName" class={`z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}>
           <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
             <div class="font-medium ">Pro User</div>
             <div class="truncate">name@flowbite.com</div>
