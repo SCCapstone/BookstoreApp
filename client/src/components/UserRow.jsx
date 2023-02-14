9cb77d6d19f6ba18d57875570e7ac6073f57a1c2
Alfred Lin <alfredl@email.sc.edu>
2023-02-14 17:28:24 -0500

    added dropdown

--- client/src/components/UserRow.jsx
+++ client/src/components/UserRow.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const UserRow = ({ contact, handleEditClick, handleDelete }) => {
+const UserRow = ({ contact, handleEditClick, handleDelete, handleUserTypeChange }) => {
+  const handleChange = (event) => {
+    const newUserType = event.target.value;
+    handleUserTypeChange(contact, newUserType);
+  }
   return (
     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
       <td class="px-6 py-4">
@@ -20,6 +24,16 @@ const UserRow = ({ contact, handleEditClick, handleDelete }) => {
         <button class="bg-persian_plum text-white ml-4 py-2 px-4" type="button" onClick={() => handleDelete(contact)}>
           Delete
         </button>
+      </td>
+      <td>
+        <select onChange={handleChange} class="form-select block w-full">
+          <option>
+            customer
+          </option>
+          <option>
+            admin
+          </option>
+        </select>
       </td>
     </tr>
   );
