c5847f90d2e531cbde6728c43b77194d0a4b2776
s-orug <orugants@email.sc.edu>
2023-02-14 17:11:34 -0500

    dropdown with state added works

--- client/src/components/ProfileIconDropdown.jsx
+++ client/src/components/ProfileIconDropdown.jsx
@@ -8,13 +8,10 @@ const ProfileIconDropdown = () => {
     <div>
       <button
         class="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:mr-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-white"
-        oncClick={() => setState(!state)}
+        onClick={() => {setState(!state); console.log("tss");}}
       >
-        <span class="sr-only">Open user menu</span>
         <FaUser
           className="w-8 h-8 mr-2 rounded-full"
-          alt="user photo"
-          
         />
         <svg
           class="w-4 h-4 mx-1.5"
