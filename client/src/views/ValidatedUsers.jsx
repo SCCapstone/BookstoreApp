e5dd155f0da48e76ae4c6ab69ffc873e5fa1c312
Alfred Lin <alfredl@email.sc.edu>
2023-02-17 09:17:51 -0500

    propsUsers

--- client/src/views/ValidatedUsers.jsx
+++ client/src/views/ValidatedUsers.jsx
@@ -21,15 +21,19 @@ export default class ValidatedUsers extends React.Component {
   }
 
   async deleteUser(user) {
-    const currentUser = this.props.currentuser;
-    if (!currentUser || !currentUser.isAdmin) {
-      return
+    const currentUser = this.props.currentUser;
+    if (!currentUser || !currentUser.isadmin) {
+      return;
     }
     const id = user._id;
     const url = "/api/users/" + id;
-    await axios.delete(url).then(() => {
-      window.location.reload();
-    }).catch((error) => console.log("Error: ", error));
+    try {
+      await axios.delete(url).then(() => {
+        window.location.reload();
+      });
+    } catch (error) {
+      console.log("Error: ", error);
+    }
   }
 
   // functionality for ensuring unauthenticated users cannot view
