3d072cb12ba00c73d5f9e29489a320c98d9ac0f4
Alfred Lin <alfredl@email.sc.edu>
2023-02-17 15:15:50 -0500

    events routes

---
+++ backend/routes/events.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/events');
+
+router.get('/events', async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/events/:id', getEvent, (req, res) => {
+  res.json(res.events);
+});
+
+router.post('/events', async (req, res) => {
+  const event = new Event({
+    title: req.body.title,
+    start: req.body.start,
+    end: req.body.end
+  });
+
+  try {
+    const newEvent = await event.save();
+    res.status(201).json(newEvent);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.patch('/events/:id', getEvent, async (req, res) => {
+  if (req.body.title != null) {
+    res.event.title = req.body.title;
+  }
+
+  if (req.body.start != null) {
+    res.event.start = req.body.start;
+  }
+
+  if (req.body.end != null) {
+    res.event.end = req.body.end;
+  }
+
+  try {
+    const updatedEvent = await res.event.save();
+    res.json(updatedEvent);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete('/events/:id', getEvent, async (req, res) => {
+  try {
+    await res.event.remove();
+    res.json({ message: 'Event deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getEvent(req, res, next) {
+  let event;
+
+  try {
+    event = await Event.findById(req.params.id);
+
+    if (event == null) {
+      return res.status(404).json({ message: 'Cannot find event' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.event = event;
+  next();
+}
+
+module.exports = router;
