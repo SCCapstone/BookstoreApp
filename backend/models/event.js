01fd695fe5f328f570a016b6cc71be4b5237b40b
Alfred Lin <alfredl@email.sc.edu>
2023-02-19 15:07:55 -0500

    file name changed

---
+++ backend/models/event.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  start: {
+    type: Date,
+    required: true
+  },
+  end: {
+    type: Date,
+    required: true
+  }
+});
+
+const Event = mongoose.model('events', eventSchema);
+
+module.exports = Event;
