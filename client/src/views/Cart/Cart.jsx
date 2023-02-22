dd27e17298c5308db07fff354fc959d402367110
s-orug <orugants@email.sc.edu>
2023-02-21 20:12:03 -0500

    update

--- client/src/views/Cart/Cart.jsx
+++ client/src/views/Cart/Cart.jsx
@@ -52,29 +52,37 @@ const Main_Cart = () => {
       <div>
         <Button onClick={() => clear_cart()}>Clear Cart</Button>
       </div>
-      <div className="grid grid-cols-1 grid-flow-row gap-2 sm:max-w-[700px] min-w-[1100px] max-w-[1150px]">
+      <div className="grid grid-cols-1 grid-flow-row min-w-[1100px] max-w-screen">
         {Object.keys(booksCartNames).map((bookName) => (
+          
+          // <div>
+          //   <div class="grid grid-cols-2 gap-2 p-5 bg-black">
+          //       <Grid item xs={4} className={`object-right w-2/4 bg-green`}>
+          //         <div class="min-w-[100px] max-w-[190px]">
+          //           <img
+          //             src={getBook(books, bookName).image}
+          //             alt=""
+          //             className="row-span-2 border-2 justify-right"
+          //           />
+          //         </div>
+          //       </Grid>
+          //     <div class="bg-red text-green-500 text-lg font-bold text-center rounded-lg w-fit mr-12">
+          //       <Grid item xs={12}>
+          //         <span class="text-center text-2xl px-16 ">
+          //           {getBook(books, bookName).name} by{" "}
+          //           {getBook(books, bookName).author}
+          //         </span>
+          //       </Grid>
+          //     </div>
+          //   </div>
+
+          //   <Grid container spacing={2}>
+          //     <ul className="row-span-6" />
+          //   </Grid>
+          // </div>
           <div>
 
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <span class="text-center text-2xl px-16">
-                  {getBook(books, bookName).name} by{" "}
-                  {getBook(books, bookName).author}
-                </span>
-              </Grid>
-              <Grid item xs={4} className={`text-right`}>
-                <div class="min-w-[100px] max-w-[190px]">
-                  <img
-                    src={getBook(books, bookName).image}
-                    alt=""
-                    className="row-span-2 border-2"
-                  />
-                </div>
-              </Grid>
 
-              <ul className="row-span-6" />
-            </Grid>
           </div>
         ))}
       </div>
