88eeac55f036c8e9a04b442552e7df4ac248e5ca
s-orug <orugants@email.sc.edu>
2023-02-22 00:24:16 -0500

    price for the cart added

--- client/src/views/Cart/Cart.jsx
+++ client/src/views/Cart/Cart.jsx
@@ -1,4 +1,6 @@
 import { CartProvider, useCart } from "react-use-cart";
+import { React, useState } from "react";
+import { Remove, Add } from "@mui/icons-material";
 import { Grid, Chip, Avatar } from "@mui/material";
 import { Button } from "@mui/material";
 import books from "../Books";
@@ -35,18 +37,49 @@ function addItem(book) {
 function getBook(books, bookName) {
   for (let i = 0; i < books.length; ++i) {
     if (books[i].name == bookName) {
-      console.log(books[i]);
       return books[i];
     }
   }
 }
 
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function calculatePrice(books, booksCartNames) {
+  let finalPrice = 0;
+  var bookNames = Object.keys(booksCartNames);
+  console.log(bookNames.length)
+  for (let i = 0; i < bookNames.length; ++i) {
+    finalPrice = finalPrice + booksCartNames[bookNames[i]]*(getBook(books, bookNames[i]).price) ;
+    // console.log(booksCartNames[bookNames[i]]);
+  }
+  // console.log(finalPrice);
+
+  return round(finalPrice,2);
+}
+
+
+
+
 const Main_Cart = () => {
   var books_cart = JSON.parse(localStorage.getItem("books_cart"));
   var booksCartNames = JSON.parse(localStorage.getItem("booksCartNames"));
   var bookNames = Object.keys(booksCartNames);
+  const bookQuantities = Object.values(booksCartNames)
 
-  console.log(books_cart);
+  const [quantity, setQuantity] = useState(bookQuantities);
+  function add(quantity) {
+    setQuantity(quantity + 1);
+  }
+  
+  function subtract(quantity) {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  }
+
+  // console.log(books_cart);
   return (
     <div>
       <div>
@@ -54,7 +87,7 @@ const Main_Cart = () => {
       </div>
       <div className="grid grid-cols-1 grid-flow-row min-w-[1100px] max-w-screen">
         {/* {Object.keys(booksCartNames).map((bookName) => ( */}
-          {/* <div>
+        {/* <div>
             <div class="grid grid-cols-2 gap-2 p-5 bg-black">
                 <Grid item xs={4} className={`object-right w-2/4 bg-green`}>
                   <div class="min-w-[100px] max-w-[190px]">
@@ -79,37 +112,63 @@ const Main_Cart = () => {
               <ul className="row-span-6" />
             </Grid>
           </div> */}
-          <div
-            className={`flex justify-center items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 flex-row flex-wrap sm:mb-20 mb-6`}
-          >
-            {Object.keys(booksCartNames).map((bookName) => (
-              <div
-                className={`flex-1 flex justify-start items-center flex-row m-3`}
-              >
-                <div class="min-w-[80px] max-w-[120px]">
-                  <img
-                    src={getBook(books, bookName).image}
-                    alt=""
-                    className="row-span-2 border-2 justify-right"
-                  />
-                </div>
-                <div>
-                  <h3 className="font-poppins font-bold xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px]  ml-3">
-                    {getBook(books, bookName).name} by{" "}
-                    {getBook(books, bookName).author}
-                  </h3>
-                  <h3 className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px]  ml-3">
-                    Price: ${getBook(books, bookName).price}
-                  </h3>
-                  <h3 className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px]  ml-3">
-                    Quantity: {booksCartNames[bookName]}
-                  </h3>
-                </div>
+        <div
+          className={`flex justify-center items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 flex-row flex-wrap sm:mb-20 mb-6`}
+        >
+          {Object.keys(booksCartNames).map((bookName) => (
+            <div
+              className={`flex-1 flex justify-start items-center flex-row m-3`}
+            >
+              <div class="min-w-[80px] max-w-[120px]">
+                <img
+                  src={getBook(books, bookName).image}
+                  alt=""
+                  className="row-span-2 border-2 justify-right"
+                />
               </div>
-            ))}
-          </div>
-          
-        {/* ))} */}
+              <div>
+                <h3 className="font-poppins font-bold xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] ml-3">
+                  {getBook(books, bookName).name} by{" "}
+                  {getBook(books, bookName).author}
+                </h3>
+                <h3 className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] ml-3">
+                  Price: ${getBook(books, bookName).price}
+                </h3>
+                <h3 className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] ml-3">
+                  Quantity: {booksCartNames[bookName]}
+                  <div className="flex pb-2 pt-2">
+          <Chip
+            avatar={
+              <Avatar onClick={() => subtract(quantity)}>
+                <Remove />
+              </Avatar>
+            }
+            label={<p className="px-2 text-lg ">{quantity}</p>}
+            clickable
+            onDelete={() => add(quantity)}
+            deleteIcon={<Add />}
+          />
+          <button className="pl-4" onClick={() => setQuantity(0)}>
+            {" "}
+            Clear{" "}
+          </button>
+        </div>
+                </h3>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div
+          className={`flex-1 flex justify-start items-center flex-row m-3 bg-camel py-4 px-4 rounded`}
+        >
+          <h4 className="font-poppins font-semibold xs:text-[30.89px] text-[25.89px] xs:leading-[43.16px] leading-[30.16px]">
+            Total: ${calculatePrice(books, booksCartNames)}
+          </h4>
+          <p className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] ml-3">
+          </p>
+        </div>
+
       </div>
     </div>
   );
