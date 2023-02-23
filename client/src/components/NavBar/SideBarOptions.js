5f03cad913f872cfa2c03cb9e3060e7b52efad61
William Hobbs <13199119+wihobbs@users.noreply.github.com>
2023-02-23 17:55:48 -0500

    fixed

--- client/src/components/NavBar/SideBarOptions.js
+++ client/src/components/NavBar/SideBarOptions.js
@@ -14,14 +14,6 @@ const admin_menu_options = [
 const customer_menu_options = [
   { name: "Home", link: "/" },
   { name: "Browse", link: "/browse" },
-  { name: "About", link: "/about" },
-  { name: "Contact Us", link: "/contact" },
-  { name: "Terms of Service", link: "/terms_of_service" },
-];
-
-const customer_menu_options = [
-  { name: "Home", link: "/" },
-  { name: "Browse", link: "/browse" },
   { name: "Forums", link: "/forums" },
   { name: "Post a Forum", link: "/createForums" },
   { name: "About", link: "/about" },
