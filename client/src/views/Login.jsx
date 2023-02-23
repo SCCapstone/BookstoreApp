8f2a2cc3d54599af786df768bf26b0366b9a5539
William Hobbs <13199119+wihobbs@users.noreply.github.com>
2023-02-23 11:07:11 -0500

    fixed console log with Jacks comments

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     try {
       const url = "/api/auth";
       const { data: res } = await axios.post(url, data);
-      console.log(res.data.data);
+      // console.log(res.data.data);
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("userType", res.data.userType);
       localStorage.setItem("userID", res.data.userID);
