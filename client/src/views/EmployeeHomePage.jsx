982f0af076ebe8fa08489d594f24b1689e4481f4
Alfred Lin <alfredl@email.sc.edu>
2023-03-06 20:54:42 -0500

    clean up

--- client/src/views/EmployeeHomePage.jsx
+++ client/src/views/EmployeeHomePage.jsx
@@ -3,23 +3,6 @@ import Calendar from 'react-calendar';
 import axios from "axios";
 import { useNavigate } from "react-router-dom"
 
-const EventInfo = ({ event, deleteEvent }) => {
-    return (
-        <div>
-            <h3>
-                {event.title}
-            </h3>
-            <p>
-                {event.start} - {event.end}
-            </p>
-            <button className="bg-black text-white p-2 mt-4 rounded hover:bg-white hover:text-black text-center" 
-                onClick = {() => deleteEvent(event)}>
-                Delete Event
-            </button>
-        </div>
-    );
-};
-
 const EmployeeHomepage = () => { 
     const [date, setDate] = useState(new Date());
     const [events, setEvents] = useState([]);
@@ -35,80 +18,13 @@ const EmployeeHomepage = () => {
             end: event.target.eventEnd.value,
         };
         setEvents([...events, newEvent]);
-        // axios.post('/api/events', newEvent)
-        // .then(response => {
-        //     setEvents([...events, response.data]);
-        // })
-        // .catch(error => {
-        //     console.error(error);
-        // });
     };
-
-    // const handleGetTodayEvents = () => {
-    //     axios.get('/api/events/today')
-    //       .then(response => {
-    //         setEvents(response.data);
-    //         setSelectedEvent(null);
-    //       })
-    //       .catch(error => {
-    //         console.error(error);
-    //       });
-    //   };
-
-    // const handleEventSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const newEvent = {
-    //       title: event.target.eventTitle.value,
-    //       start: event.target.eventStart.value,
-    //       end: event.target.eventEnd.value,
-    //     };
-    //     try {
-    //       // Send the new event to the server
-    //       const response = await axios.post("/api/events", newEvent);
-    //       // Add the new event to the events array
-    //       setEvents([...events, response.data]);
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   };
-
-//     const handleEventDelete = (eventToDelete) => {
-//         axios.delete(`/api/events/${eventToDelete.id}`)
-//     .then(response => {
-//     setEvents(events.filter(event => event.id !== eventToDelete.id));
-//     if (selectedEvent && selectedEvent.id === eventToDelete.id) {
-//       setSelectedEvent(null);
-//     }
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-//         if (selectedEvent && selectedEvent === eventToDelete) {
-//             setSelectedEvent(null);
-//         }
-//     };
-
     const handleEventDelete = (eventToDelete) => {
         setEvents(events.filter(event => event !== eventToDelete));
         if (selectedEvent && selectedEvent === eventToDelete) {
             setSelectedEvent(null);
         }
     };
-
-    // const handleEventDelete = async (eventToDelete) => {
-    //     try {
-    //     // Send a DELETE request to the server to delete the event
-    //     await axios.delete(`/api/events/${eventToDelete.id}`);
-    //     // Remove the event from the events array
-    //     setEvents(events.filter((event) => event.id !== eventToDelete.id));
-    //     // Clear the selected event if it's the one being deleted
-    //     if (selectedEvent && selectedEvent.id === eventToDelete.id) {
-    //         setSelctedEvent(null);
-    //     }
-    //     } catch (error) {
-    //     console.error(error);
-    //     }
-    // };
 
     return (
 
@@ -132,13 +48,7 @@ const EmployeeHomepage = () => {
                         const eventDatesE = eventDatesEnd[index];
                         return date >= eventDatesStart && date <= eventDatesE;
                     });
-                    // if (eventDatesStart.some(eventDate => eventDate.toDateString() == date.toDateString())) {
-                    //     return "bg-blue-500 text-white border border-gray"
-                    // } if (eventDatesEnd.some(eventDate => eventDate.toDateString() == date.toDateString())) {
-                    //     return "bg-blue-500 text-white border border-gray"
-                    // } else {
-                    //     return "border boder-gray"
-                    // }
+
                     const firstDay = eventDatesStart.some(eventDate => eventDate.toDateString() == date.toDateString());
 
                     if (withinStartEnd || firstDay) {
@@ -148,8 +58,6 @@ const EmployeeHomepage = () => {
                     }
                 }}
                 tileContent={({date,view}) => {
-                    // const event = events.find(event => new Date(event.start).toDateString() === date.toDateString());
-
                     const eventsThatStartOnDate = events.filter(
                         event => new Date(event.start).toDateString() === date.toDateString()
                     );
@@ -201,69 +109,8 @@ const EmployeeHomepage = () => {
                         </div>
                       );
 
-                    // if (event) {
-                    //     const isSelected = selectedEvent && selectedEvent.start === event.start;
-                    //     return (
-                    //         <div
-                    //             onClick={() => {
-                    //                 if (isSelected) {
-                    //                     setSelctedEvent(null);
-                    //                 } else {
-                    //                     setSelctedEvent(event)
-                    //                 }
-                    //             }}
-                    //             // onMouseOver={() => setSelctedEvent(event)}
-                    //             // onMouseOut={()=> setSelctedEvent(null)}
-                    //         >
-                    //             <h3>
-                    //                 {event.title}
-                    //             </h3>
-                    //         </div>
-                    //     );
-                    // } 
-                    // else {
-                    //     return;
-                    // }
-
-                    // if (eventsThatStartOnDate.length > 0 || eventsThatSpanAcrossDate.length > 0) {
-                    //     const isSelected =
-                    //       selectedEvent &&
-                    //       selectedEvent.start === eventsThatStartOnDate[0]?.start &&
-                    //       selectedEvent.end === eventsThatStartOnDate[0]?.end;
-                    
-                    //     return (
-                    //       <div
-                    //         onClick={() => {
-                    //           if (isSelected) {
-                    //             setSelctedEvent(null);
-                    //           } else {
-                    //             setSelctedEvent(eventsThatStartOnDate[0]);
-                    //           }
-                    //         }}
-                    //         className={`${
-                    //           isSelected ? 'bg-blue-500 text-white' : ''
-                    //         } border border-gray p-1 rounded`}
-                    //       >
-                    //         {eventsThatStartOnDate.length > 0 && (
-                    //         <p className="mb-1">
-                    //             {eventsThatStartOnDate[0].title}
-                    //         </p>
-                    //         )}
-                    //         {eventsThatSpanAcrossDate.length > 0 && (
-                    //         <p className="text-sm italic">
-                    //             {eventsThatSpanAcrossDate.map(event => event.title).join(', ')}
-                    //         </p>
-                    //         )}
-                    //       </div>
-                    //     );
-                    //   }
-                    // return null;
                 }}
             />
-            {/* {selectedEvent && (
-                <EventInfo event={selectedEvent} deleteEvent = {handleEventDelete}/>
-            )} */}
-           
         </div>
             <form onSubmit={handleEventSubmit} className="mt-4">
                 <label className="block font-medium mb-2">
@@ -291,7 +138,6 @@ const EmployeeHomepage = () => {
                     className="w-full p-2 border rounded"
                 />
                 <button 
-                    // onClick={handleEventSubmit}
                     type="submit" 
                     className="bg-black text-white p-2 mt-4 rounded hover:bg-white hover:text-black text-center"
                 >
