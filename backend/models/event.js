bf2cb369b5c0f26c5775d78e73016dccf5b81d57
Alfred Lin <alfredl@email.sc.edu>
2023-03-06 19:43:03 -0500

    added

---
+++ backend/models/event.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Joi = require("joi");
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  start: { type: Date, required: true },
+  end: { type: Date, required: true },
+});
+
+const Event = mongoose.model("events", eventSchema);
+
+const validate = (data) => {
+  const schema = Joi.object ({
+    title: Joi.string().required().label("Title of the Event"),
+    start: Joi.date().required().lablel("Start of the Event"),
+    end: Joi.date().required().label("End of the Event"),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { Event, validate };
