1d1a9c6566d18aaffb62858c0fa6870e3dfbbee1
Alfred Lin <alfredl@email.sc.edu>
2023-03-07 21:05:57 -0500

    changes

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,79 +1,32 @@
 const router = require("express").Router();
 const { Event, validate } = require("../models/event");
 
-router.get('/events', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const events = await Event.find();
-    res.json(events);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const newEvent = new Event({
+      title: req.body.title,
+      start: req.body.start,
+      end: req.body.end
+    });
+    const savedEvent = await newEvent.save();
+    res.json(savedEvent);
+  } catch (error) {
+    res.status(500).json({ message: "error message" });
   }
 });
 
-router.get('/events/:id', getEvent, (req, res) => {
-  res.json(res.events);
-});
-
-router.post('/events', async (req, res) => {
-  const event = new Event({
-    title: req.body.title,
-    start: req.body.start,
-    end: req.body.end
-  });
-
+// Route to delete an event
+router.delete('/events/:id', async (req, res) => {
   try {
-    const newEvent = await event.save();
-    res.status(201).json(newEvent);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-router.patch('/events/:id', getEvent, async (req, res) => {
-  if (req.body.title != null) {
-    res.event.title = req.body.title;
-  }
-
-  if (req.body.start != null) {
-    res.event.start = req.body.start;
-  }
-
-  if (req.body.end != null) {
-    res.event.end = req.body.end;
-  }
-
-  try {
-    const updatedEvent = await res.event.save();
-    res.json(updatedEvent);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-router.delete('/events/:id', getEvent, async (req, res) => {
-  try {
-    await res.event.remove();
-    res.json({ message: 'Event deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-async function getEvent(req, res, next) {
-  let event;
-
-  try {
-    event = await Event.findById(req.params.id);
-
-    if (event == null) {
-      return res.status(404).json({ message: 'Cannot find event' });
+    const deletedEvent = await Event.findByIdAndDelete(req.params.id);
+    if (!deletedEvent) {
+      res.status(404).json({ message: 'Event not found' });
+      return;
     }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
+    res.json(deletedEvent);
+  } catch (error) {
+    console.log(error);
   }
-
-  res.event = event;
-  next();
-}
+});
 
 module.exports = router;
