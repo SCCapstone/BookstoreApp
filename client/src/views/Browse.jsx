63a77bc2570de96bd59c71de5575c016d8fe719a
Jack <jackcoberman@gmail.com>
2023-03-08 00:26:34 -0500

    Search Books works, but book page generator does not

--- client/src/views/Browse.jsx
+++ client/src/views/Browse.jsx
@@ -34,22 +34,27 @@ class Browse extends Component {
       }
     });
   }
-  // const book_instance = localStorage.getItem("Book");
 
   changeOption(option) {
     this.setState((state) => ({
       value: option
     }));
-    // localStorage.setItem("option", option);
   }
 
-  // let prevOption = localStorage.getItem("option");
-  // if (prevOption) {
-  //   setValue(prevOption);
-  // }
-  // console.log(prevOption);
-  // console.log(prevOption);
-
+  filterOptions = (options, state) => {
+    let newOptions = [];
+    options.forEach((element) => {
+      const baseTitle = element.title.replace(",", "").toLowerCase();
+      const baseAuthor = element.author.replace(",", "").toLowerCase();
+      const baseGenre = element.genre[0].replace(",","").toLowerCase();
+      const baseInput = state.inputValue.toLowerCase();
+      if (baseTitle.includes(baseInput) ||
+          baseAuthor.includes(baseInput) ||
+          baseGenre.includes(baseInput))
+        newOptions.push(element);
+    });
+    return newOptions;
+  };
 
   render() {
     return (
@@ -60,37 +65,12 @@ class Browse extends Component {
           </div>
         </div>
 
-        {/* <div class=" xl:w-8/12 lg:w-8/12 md:w-4/12 w-4/12 py-2 grid">
-          <p className={`${styles.paragraph} max-w-[1158px] relative grid gap-2`}>
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </div>
-            <input
-              className="rounded border bg-white py-2 grid max-w-[1158px] px-4 w-[500px] xl:w-[1100px] lg:w-[600px] md:w-[500px] pl-10"
-              placeholder="Search Books"
-            />
-          </p>
-        </div> */}
-
         <div class="flex pb-2">
           <Autocomplete
             className="rounded border bg-white py-2 max-w-[1158px] px-4 w-[500px] xl:w-[1100px] lg:w-[600px] md:w-[500px]"
             value={this.state.book}
             onChange={(event, newValue) => {
+              window.location = `/${newValue.author}/${newValue.title}`;
               this.setState((state) => ({
                   book: newValue
               }));
@@ -103,6 +83,8 @@ class Browse extends Component {
             }}
             options={this.state.books}
             getOptionLabel={(option) => option.title}
+            groupBy={(option) => option.genre[0] }
+            filterOptions={this.filterOptions}
             renderInput={(params) => (
                 <TextField
                     {...params}
@@ -128,12 +110,6 @@ class Browse extends Component {
             <option value="Price: low to high">Price: low to high</option>
             <option value="Price: high to low">Price: high to low</option>
           </select>
-
-          {/* <select className="w-1/6 ... bg-white border ">
-            <option selected disabled hidden>
-              Genre
-            </option>
-          </select> */}
         </div>
 
         <div>
