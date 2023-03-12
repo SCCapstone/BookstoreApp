55ae7db286a2c1d2cbf7acffca39f73a0a3a04f0
Rahul Bulusu <rbulusu@email.sc.edu>
2023-03-12 11:39:35 -0400

    changed the tag from profile to manage account

--- client/src/components/ProfileIcon.jsx
+++ client/src/components/ProfileIcon.jsx
@@ -72,7 +72,7 @@ export default function ProfileIcon() {
               href="/my_account"
               class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
-              Profile
+              Manage Account
             </a>
           </li>
         </MenuItem>
