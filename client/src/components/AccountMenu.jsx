6aa6ecaa9553cbdf387f97aaa41e8fabc212e459
Rahul Bulusu <rbulusu@email.sc.edu>
2023-03-11 22:22:29 -0500

    fixed import of react

--- client/src/components/AccountMenu.jsx
+++ client/src/components/AccountMenu.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
