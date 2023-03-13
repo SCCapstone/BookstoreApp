44daf5da3e449d74b8f35b8b5d793d6c7ba46413
s-orug <orugants@email.sc.edu>
2023-03-13 15:00:55 -0400

    can read the balance

--- client/src/components/UserRow.jsx
+++ client/src/components/UserRow.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserRow = ({ contact, handleEditClick, handleDelete }) => {
   // console.log(contact)
-
+  const [balance, setBalance] = useState(contact.balance["$numberDecimal"]);
+  console.log(balance)
   const handleChange = (e) => {
     const newUserType = e.target.value;
     handleEditClick(contact, newUserType);
-  }
+  };
   return (
     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
       <td class="px-6 py-4">
@@ -14,7 +15,9 @@ const UserRow = ({ contact, handleEditClick, handleDelete }) => {
           ? contact.fullName
           : contact.firstName + " " + contact.lastName}
       </td>
-      <td class="px-6 py-4">{contact.username ? contact.username : contact._id}</td>
+      <td class="px-6 py-4">
+        {contact.username ? contact.username : contact._id}
+      </td>
       <td class="px-6 py-4">{contact.email}</td>
       <td>
         {/* <button
@@ -23,29 +26,31 @@ const UserRow = ({ contact, handleEditClick, handleDelete }) => {
         >
           Edit
         </button> */}
-        <button class="bg-persian_plum text-white ml-4 py-2 px-4" type="button" onClick={() => handleDelete(contact)}>
+        <button
+          class="bg-persian_plum text-white ml-4 py-2 px-4"
+          type="button"
+          onClick={() => handleDelete(contact)}
+        >
           Delete
         </button>
       </td>
       <td>
-        <select value={contact.role} onChange={handleChange} class="form-select block w-full">
-          <option value="customer">
-            customer
-          </option>
-          <option value="admin">
-            admin
-          </option>
+        <select
+          value={contact.role}
+          onChange={handleChange}
+          class="form-select block w-full"
+        >
+          <option value="customer">customer</option>
+          <option value="admin">admin</option>
         </select>
       </td>
       <td>
-        <select value={contact.role} onChange={handleChange} class="form-select block w-full">
-          <option value="customer">
-            customer
-          </option>
-          <option value="admin">
-            admin
-          </option>
-        </select>
+        <input
+          type="number"
+          value={balance}
+          className="text-black"
+        />
+        {/* ${balance["$numberDecimal"]} */}
       </td>
     </tr>
   );
