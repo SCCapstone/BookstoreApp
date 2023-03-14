c1aa8c7aa582ba87dae737c74a48066a33ecfea3
Rahul Bulusu <rbulusu@email.sc.edu>
2023-03-14 18:53:29 -0400

    changed wishlist to favorites

--- client/src/components/ProfileIcon.jsx
+++ client/src/components/ProfileIcon.jsx
@@ -133,7 +133,7 @@ export default class ProfileIcon extends Component {
                     href="/favorites"
                     class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
-                    Wishlist
+                    Favorites
                   </a>
                 </li>
               </MenuItem>
