8078ae6cedf8cfd63896b7c3cd90b77d7a652b78
Alfred Lin <alfredl@email.sc.edu>
2023-03-19 15:46:03 -0400

    finished

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const jwt = require("jsonwebtoken")
 const Joi = require("joi");
 
-const eventSchema = new mongoose.Schema({
+const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true },
+  post: { type: String, required: true },
 });
 
-const Event = mongoose.model("blogs", blogSchema);
+const Blog = mongoose.model("blogs", blogSchema);
 
 const validate = (data) => {
   const schema = Joi.object ({
     title: Joi.string().required().label("Title of the Event"),
-    content: Joi.string().required().label("Content of the Event"),
+    post: Joi.string().required().label("Content of the Event"),
   });
   return schema.validate(data);
 };
