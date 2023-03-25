d3b6ebfffd77cc5014662665290e15682a99e8f3
Rahul Bulusu <rbulusu@email.sc.edu>
2023-03-25 11:19:41 -0400

    added comments to Aboutus Page

--- client/src/views/Aboutus.jsx
+++ client/src/views/Aboutus.jsx
@@ -1,18 +1,19 @@
-//imports 
 import React from "react";
-import book_dispensary from "../assets/book_dispensary.jpg";
+import book_dispensary from "../assets/book_dispensary.jpg"; //picture of the book dispensary - found off the internet
 import { styles } from "../styles";
 
 const Aboutus = () => {
-  var books_cart = JSON.parse(localStorage.getItem("books_cart"));
-  console.log(books_cart);
+  var books_cart = JSON.parse(localStorage.getItem("books_cart")); //gets the book from the local storage and stores it as an item in the books cart to show the item type and number of items (in this case item: book) in the cart
+  console.log(books_cart); //outputs what's in the books cart to the console (when running the file use inspect element and go to console to see this when clicking the cart)
   return (
+    //About Us Block
     <section>
       <div class="py-4">
         <div class="grid bg-polished_pine text-center text-white border-black border-2 text-3xl rounded py-3 ">
           About us
         </div>
       </div>
+      {/*paragraph block with a description of what the Book Dispensary is and what it has to offer*/}
       <div class="xl:ml-20 xl:w-8/12 lg:w-8/12 md:w-4/12 w-4/12 py-2 grid grid-rows-auto max-w-[1400px]">
         <p className={`${styles.paragraph} max-w-[1158px]`}>
           The Book Dispensary is a local, family owned business that began life
@@ -30,6 +31,7 @@ const Aboutus = () => {
           alt="Book Dispensary"
           className="row-span-2"
         />
+        {/*Location of the Book Dispensary */}
         <p className={`${styles.paragraph}`}>
           Location to Visit Us: 710-C Gracern Rd, Columbia, SC 29210
         </p>
