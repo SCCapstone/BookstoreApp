5c9963aafb227797bbd3a5861441d012c43312f9
Rahul Bulusu <rbulusu@email.sc.edu>
2023-03-26 10:33:19 -0400

    comments to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import CompleteNavbar from "./components/NavBar";
-import { BrowserRouter, Routes } from "react-router-dom";
-import { CartProvider } from "react-use-cart";
+import CompleteNavbar from "./components/NavBar"; //has the side nav bar and the corresponding views pages which appear which are in index.jsx and SideBar.jsx and the top nav bar which is also utilized to make the complete navbar
+import { BrowserRouter, Routes } from "react-router-dom"; //used to route to different pages
+import { CartProvider } from "react-use-cart"; //for the cart to check the books
 import Main_Cart from "./views/Cart/Cart";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; //use state feature
 
+//functions as the "main" of the App where the side nav bar with the view components and the top nav bar will appear on every page when on a different view
+//it is functioned through using routes
 function App() {
   return (
-    <div className="bg-gainsboro h-screen">
+    <div className="bg-gainsboro h-screen"> {/*the home screen's set hard-coded default color based on our design from Figma*/}
       <CompleteNavbar />
       <BrowserRouter>
         <Routes></Routes>
