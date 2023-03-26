1b6d7acd868f174dd1c661a9d5e608f89c2c22de
Rahul Bulusu <rbulusu@email.sc.edu>
2023-03-26 10:43:59 -0400

    added comments to navbar.cy.js

--- client/cypress/e2e/navbar.cy.js
+++ client/cypress/e2e/navbar.cy.js
@@ -1,8 +1,9 @@
+//one of our tests using cypress for the login 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('localhost:3001/login')
+    cy.visit('localhost:3001/login') //starts the app and goes to the login
 
-    cy.get('input[type="text"]').type('trial@error.com')
-    cy.get('input[type="password"]').type('trial{enter}')
+    cy.get('input[type="text"]').type('trial@error.com') //username for the login
+    cy.get('input[type="password"]').type('trial{enter}') //password for the login
   })
 })
