040472003abee6e1b3c7d5a449e2fd9aeefab648
Jack <jackcoberman@gmail.com>
2023-03-29 00:17:20 -0400

    profile dropdown works with initials

--- client/src/components/ProfileIcon.jsx
+++ client/src/components/ProfileIcon.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
@@ -7,42 +7,29 @@ import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import axios from 'axios';
-export default class ProfileIcon extends Component {
+export default function ProfileIcon(currentUser) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [user, setUser] = useState({});
+  const open = Boolean(anchorEl);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        user: {},
-        anchorEl: true,
-        open: true,
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "/api/users/" + currentUser.currentUser;
+      let res = await axios.get(url);
+      res.data.password = "";
+      setUser(res.data);
     };
-  };
+    fetchData();
+  },[]);
 
-  async componentDidMount() {
-    const url = "/api/users/" + this.props.currentUser;
-
-      await axios.get(url).then(res => {
-          let user = res.data;
-          user.password = "";
-
-          this.setState((state) => ({
-              user: user 
-          }));
-      });
-  }
-
-  setAnchorEl(anchorEl) {
-    this.setState((state) => ({
-      anchorEl: anchorEl
-    }));
-  };
-
-  login () {
-    window.location.href = "/login";
-    console.log("login");
-  };
-
-  logout () {
+  const logout = () => {
       localStorage.removeItem("token");
       localStorage.removeItem("userType");
       localStorage.removeItem("userID");
@@ -51,118 +38,126 @@ export default class ProfileIcon extends Component {
       console.log("logout");
   };
   
-  isLoggedIn () {
-    const currentUser = this.props.currentUser;
-    return currentUser && currentUser.length !== 0;
+  const isLoggedIn = () => {
+    return currentUser?.currentUser?.length !== 0 && currentUser.currentUser !== null;
   };
 
-  render() {
-    return this.isLoggedIn() ? (
-          <div class="py-4">
-            <React.Fragment>
-              <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-                <Tooltip title="Account settings">
-                  <IconButton
-                    onClick={this.setAnchorEl(!this.state.anchorEl)}
-                    size="small"
-                    sx={{ ml: 2 }}
-                    aria-controls={this.state.open ? 'account-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={this.state.open ? 'true' : undefined}
-                  >
-                    <Avatar sx={{ width: 33, height: 33 }}>{this.state.user.firstName + this.state.user.lastName}</Avatar>
-                  </IconButton>
-                </Tooltip>
-              </Box>
-              <Menu
-                anchorEl={this.state.anchorEl}
-                id="account-menu"
-                open={this.state.open}
-                onClose={this.setAnchorEl(true)}
-                onClick={this.setAnchorEl(false)}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: 'visible',
-                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                    mt: 1.5,
-                    '& .MuiAvatar-root': {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    '&:before': {
-                      content: '""',
-                      display: 'block',
-                      position: 'absolute',
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: 'background.paper',
-                      transform: 'translateY(-50%) rotate(45deg)',
-                      zIndex: 0,
-                    },
-                  },
-                }}
-                transformOrigin={{ horizontal: 'right-center', vertical: 'top-center' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'top-center' }}
-              >
-                <MenuItem onClick={this.setAnchorEl(false)}>
-                  <li>
-                    <a
-                      href="/my_account"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Manage Account
-                    </a>
-                  </li>
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={this.setAnchorEl(false)}>
-                  <li>
-                    <a
-                      href="/cart"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Cart & Earnings
-                    </a>
-                  </li>
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={this.setAnchorEl(false)}>
-                  <li>
-                    <a
-                      href="/favorites"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Favorites
-                    </a>
-                  </li>
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={this.setAnchorEl(false)}>
-                  <li>
-                  <a
-                      href="/"
-                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Logout
-                    </a>
-                  </li>
-                </MenuItem>
-              </Menu>
-            </React.Fragment>
-          </div>
-        ): (
-          <div>
-            <button class="">
-              <a className="text-white text-xl" href="/login">
-                Login
-              </a> 
-            </button>
-          </div>
-        );
-    };
+  const userInitials = () => {
+    let initials = "";
+    if (user?.firstName?.length > 0) {
+      initials += user.firstName[0];
+      if (user?.lastName.length>0) {
+        initials += user.lastName[0];
+      }
+    } else {
+      initials = "##";
+    }
+    return initials.toUpperCase();
+  }
+
+  return isLoggedIn() ? (
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 33, height: 33 }}>{userInitials()}</Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right-center', vertical: 'top-center' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top-center' }}
+      >
+        <MenuItem onClick={handleClose}>
+          <li>
+            <a
+              href="/my_account"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Manage Account
+            </a>
+          </li>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <li>
+            <a
+              href="/cart"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Cart & Earnings
+            </a>
+          </li>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <li>
+            <a
+              href="/favorites"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Favorites
+            </a>
+          </li>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={logout}>
+          <li>
+          <a
+              href="/"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Logout
+            </a>
+          </li>
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
+    ): (
+    <div>
+      <button class="">
+        <a className="text-white text-xl" href="/login">
+          Login
+        </a> 
+      </button>
+    </div>
+  );
 };
