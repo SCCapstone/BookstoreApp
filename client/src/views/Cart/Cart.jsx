f1adfdaa1d55f0106e76fb5f18816b5c73bc964d
s-orug <orugants@email.sc.edu>
2023-03-30 16:18:38 -0400

    unknown changes

--- client/src/views/Cart/Cart.jsx
+++ client/src/views/Cart/Cart.jsx
@@ -7,12 +7,14 @@ import { Button } from "@mui/material";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { cartChange } from "../../components/NavBar/NavBar";
+import swal from "sweetalert2";
 
 cartChange();
 
 function clear_cart() {
   localStorage.setItem("books_cart", JSON.stringify([]));
   localStorage.setItem("booksCartNames", JSON.stringify({}));
+
   window.location.reload(false);
 }
 
@@ -39,12 +41,9 @@ function addItem(book) {
 }
 
 function getBook(books, bookName) {
-  // console.log(books);
-  // console.log(bookName);
   for (let i = 0; i < books.length; ++i) {
     if (books[i].title == bookName) {
-      console.log(books);
-      console.log(bookName);
+      // console.log(books[i]);
       return books[i];
     }
   }
@@ -57,6 +56,7 @@ function round(value, decimals) {
 function getKeys(obj) {
   var keys = [];
   iterate(obj, function (oVal, oKey) {
+    // window.location.reload();
     keys.push(oKey);
   });
   return keys;
@@ -65,6 +65,7 @@ function iterate(iterable, callback) {
   for (var key in iterable) {
     if (
       key === "length" ||
+      // window.location.reload();
       key === "prototype" ||
       !Object.prototype.hasOwnProperty.call(iterable, key)
     )
@@ -86,30 +87,22 @@ function calculatePrice(books, booksCartNames) {
 }
 
 const MainCart = ({ currentUser }) => {
-  var books = [];
-  var tempBooksState = [];
-  const fetchBooks = async () => {
-    // return fetch("/api/books")
-    //   .then((response) => response.json())
-    //   .then((data) => setBooks(data))
-    //   .then(console.log(response));
-    // const url = "/api/books/";
-    // const res = await axios.get(url, data);
-    // return res.data;
 
-    const res = await fetch("http://127.0.0.1:5500/fff") ;
-  const result = await res.json();
-  const miro = result;
-  console.log(miro);
-  };
-  tempBooksState = fetchBooks();
+  const books = JSON.parse(localStorage.getItem("books"));
 
-  tempBooksState.then((value) => {
-    books = value
-
-  });
+  // const [books, setBooks] = useState([]);
+  // const fetchBooks = () => {
+  //   return fetch("/api/books")
+  //     .then((response) => response.json())
+  //     .then((data) => setBooks(data));
+  // };
+  // useEffect(() => {
+  //   fetchBooks();
+  // }, []);
 
 
+
+  // console.log(books)
   const navigate = useNavigate();
   var booksCartNames = JSON.parse(localStorage.getItem("booksCartNames"));
   localStorage.setItem("cartItemsQuantity", findQuantity(booksCartNames));
@@ -206,7 +199,7 @@ const MainCart = ({ currentUser }) => {
   function userData(users, currentUser) {
     for (let i = 0; i < users.length; ++i) {
       if (users[i]._id === currentUser) {
-        return users[i].balance.$numberDecimal;
+        return round(users[i].balance.$numberDecimal, 2);
       }
     }
   }
@@ -217,13 +210,58 @@ const MainCart = ({ currentUser }) => {
     currentBalance = userData(allUsers, currentUser);
   }
 
+  function userInfo(users, currentUser) {
+    for (let i = 0; i < users.length; ++i) {
+      if (users[i]._id === currentUser) {
+        return users[i];
+      }
+    }
+  }
+
+  const handleSubmitOrder = async (e) => {
+    if (currentBalance - calculatePrice(books, booksCartNames) >= 0) {
+      e.preventDefault();
+      try {
+        const url = "/api/orders";
+        axios.post(url, orderSetter()).then((res) => {
+          console.log(res.status);
+        });
+      } catch (error) {
+        if (error.response?.status >= 400 && error.response.status <= 500) {
+          setError(error.response.data.message);
+        }
+      }
+    }
+  };
+
+  const [userBalance, setUserBalance] = useState(0);
+
   const availableBalance = () => {
     if (currentUser && currentUser.length !== 0) {
       var allUsers = ValidatedUsers(currentUser);
-      return userData(allUsers, currentUser);
+      var tmp = userData(allUsers, currentUser);
+      return tmp;
     }
     return 0;
   };
+
+  function orderSetter() {
+    var user = {};
+    for (let i = 0; i < allUsers.length; ++i) {
+      if (allUsers[i]._id === currentUser) {
+        user["firstName"] = allUsers[i].firstName;
+        user["lastName"] = allUsers[i].lastName;
+        user["email"] = allUsers[i].email;
+        user["role"] = allUsers[i].role;
+      }
+    }
+    user["order"] = booksCartNames;
+    user["orderPrice"] = calculatePrice(books, booksCartNames);
+    user["orderDate"] = new Date().toLocaleString();
+    user["orderStatus"] = "In-Progress"
+    console.log(user);
+    return user;
+  }
 
   var puchaseBooks = (currentUser) => {
     if (currentUser && currentUser.length !== 0) {
@@ -231,8 +269,13 @@ const MainCart = ({ currentUser }) => {
         if (currentBalance - calculatePrice(books, booksCartNames) === 0) {
           data.balance = "0";
         } else {
-          data.balance = currentBalance - calculatePrice(books, booksCartNames);
+          data.balance = round(
+            currentBalance - calculatePrice(books, booksCartNames),
+            2
+          );
         }
+        orderSetter();
+        console.log(userData(allUsers, currentUser));
         localStorage.setItem("books_cart", JSON.stringify([]));
         localStorage.setItem("booksCartNames", JSON.stringify({}));
         handleChange();
@@ -246,9 +289,9 @@ const MainCart = ({ currentUser }) => {
   };
 
   return (
-    <div>
+    <div class="py-6">
       <Grid item xs={12}>
-        <span class="text-center text-3xl px-16 py-3">
+        <span class="text-center text-3xl px-16 py-6 mt-10">
           Available balance: ${availableBalance()}
         </span>
       </Grid>
@@ -316,7 +359,10 @@ const MainCart = ({ currentUser }) => {
         </div>
         <form
           className={`grid grid-cols-3 flex-1 flex justify-start items-center  m-3 bg-camel py-4 px-4 rounded min-w-[500px] max-w-[600px] gap-16`}
-          onSubmit={handleSubmit}
+          onSubmit={(e) => {
+            handleSubmit(e);
+            handleSubmitOrder(e);
+          }}
         >
           <h4 className="col-span-1 font-poppins font-semibold xs:text-[30.89px] text-[25.89px] xs:leading-[43.16px] leading-[30.16px]">
             Total: ${calculatePrice(books, booksCartNames)}
