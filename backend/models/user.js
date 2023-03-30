b667aabe933dd5eea0fcaa62486eed65da011332
Alfred Lin <alfredl@email.sc.edu>
2023-03-30 17:49:43 -0400

    back to original

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,7 +8,6 @@ const userSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  // password: passwordComplexity().required().label("Password"),
   role: { type: String, required: true, enum: ["admin", "customer"]},
   balance: { type: mongoose.Schema.Types.Decimal128, required: false },
   
@@ -24,30 +23,16 @@ userSchema.methods.generateAuthToken = function () {
 const User = mongoose.model("user", userSchema);
 
 const validate = (data) => {
-  const complexityOptions = {
-    min: 8,
-    max: 30,
-    lowerCase: 1,
-    upperCase: 1,
-    numeric: 1,
-    symbol: 1,
-  };
-  const passwordComplexitySchema = passwordComplexity(complexityOptions)
-    .required()
-    .label("Password")
-    .message(
-      "Password should be 8-30 characters long and should contain at least one lowercase letter, one uppercase letter, one number, and one special character."
-    );
   const schema = Joi.object({
     firstName: Joi.string().required().label("First Name"),
     lastName: Joi.string().required().label("Last Name"),
     email: Joi.string().required().label("Email"),
-    password: passwordComplexitySchema,
+    password: Joi.string().required().label("Password"),
+    // password: passwordComplexity.required().label("Password")
     role: Joi.string().required().valid("admin", "customer").label("Role"),
     balance: Joi.number().required().label("Balance"),
   });
   return schema.validate(data);
-
 };
 
 module.exports = { User, validate };
