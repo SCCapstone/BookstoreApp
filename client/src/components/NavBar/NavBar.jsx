0207e280367f8df58430400098ecb7e91088751e
Alfred Lin <alfredl@email.sc.edu>
2023-04-07 19:28:22 -0400

    remove cart

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,17 +4,8 @@ import { IoMdCart } from "react-icons/io";
 
 import navbar_logo from "../NavBar/navbar_logo.jpg"
 
-import { ButtonGroup, Button, AddIcon, RemoveIcon } from "@mui/material";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Badge } from "@mui/material";
 import ProfileIcon from "../ProfileIcon";
-
-let val = 0;
-
-function cartChange() {
-  var booksCartNames = JSON.parse(localStorage.getItem("booksCartNames"));
-  val = findQuantity(booksCartNames);
-}
 
 function findQuantity(booksCartNames) {
   let count = 0;
@@ -26,14 +17,14 @@ function clearCart() {
   localStorage.removeItem("booksCartNames");
 }
 
-const NavBar = ({ user, items, isLoggedIn }) => { // add isLoggedIn prop to indicate if the user is logged in
+const NavBar = ({ user, items, isLoggedIn }) => {
   const navigate = useNavigate();
   const [shoppingCartQuantity, setShoppingCartQuantity] = useState(0);
 
   useEffect(() => {
-    if (val) {
-      setShoppingCartQuantity(val);
-    }
+    const booksCartNames = JSON.parse(localStorage.getItem("booksCartNames"));
+    const count = findQuantity(booksCartNames);
+    setShoppingCartQuantity(count);
   }, []);
 
   useEffect(() => {
@@ -43,23 +34,29 @@ const NavBar = ({ user, items, isLoggedIn }) => { // add isLoggedIn prop to indi
     }
   }, [isLoggedIn]);
 
+  function handleCartChange() {
+    const booksCartNames = JSON.parse(localStorage.getItem("booksCartNames"));
+    const count = findQuantity(booksCartNames);
+    setShoppingCartQuantity(count);
+  }
+
   return (
     <div>
-      <nav class="bg-persian_plum px-4 sm:px-6 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b-4 border-gray-200 dark:border-gray-600">
-        <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <nav className="bg-persian_plum px-4 sm:px-6 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b-4 border-gray-200 dark:border-gray-600">
+        <div className="container flex flex-wrap items-center justify-between mx-auto">
           <span className="flex gap-8">
-            <a href="/" class="flex items-center cols-span-8">
+            <a href="/" className="flex items-center cols-span-8">
               <img
-              src={navbar_logo}
-              alt="BookStore Logo"
-              className="row-span-3"
-            />
+                src={navbar_logo}
+                alt="BookStore Logo"
+                className="row-span-3"
+              />
             </a>
           </span>
-          <div class="flex md:order-2 grid grid-cols-5">
-            {isLoggedIn && ( // render cart icon only if user is logged in
+          <div className="flex md:order-2 grid grid-cols-5">
+            {isLoggedIn && (
               <Badge
-                badgeContent={val}
+                badgeContent={shoppingCartQuantity}
                 color="primary"
                 className=""
                 anchorOrigin={{ vertical: "top", horizontal: "right" }}
@@ -86,4 +83,4 @@ const NavBar = ({ user, items, isLoggedIn }) => { // add isLoggedIn prop to indi
   );
 };
 
-export { NavBar, cartChange };
+export { NavBar, handleCartChange };
