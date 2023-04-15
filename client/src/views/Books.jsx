facb40a2a9f8292fe9aaf3ed05def0befb132f9a
s-orug <orugants@email.sc.edu>
2023-04-15 01:51:36 -0400

    restore #1

--- client/src/views/Books.jsx
+++ client/src/views/Books.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import React from "react";
-import UserRow from "../components/UserRow";
 import { Pagination } from "@mui/material";
 import { isAdmin, sendToHome } from "../utils/PermissionUtils";
+import BookRow from "../components/BookRow";
 
 export default class Books extends React.Component {
   state = {
-    users: [],
+    books: [],
     pageSize: 12,
     currentPage: 1
   };
@@ -14,47 +14,48 @@ export default class Books extends React.Component {
   async getBooks() {
     const url = "/api/books";
     axios.get(url).then((res) => {
-      const users = res.data;
-      this.setState({ users: users });
+      const books = res.data;
+      this.setState({ books: books });
     });
   }
 
   componentDidMount() {
     this.getBooks();
+    console.log(this.state.books)
   }
 
-  async deleteBook(book) {
-    const id = book._id;
-    const url = "/api/books/" + id;
-    await axios
-      .delete(url)
-      .then(() => {
-        window.location.reload();
-      })
-      .catch((error) => console.log("Error: ", error));
-  }
+  // async deleteBook(book) {
+  //   const id = book._id;
+  //   const url = "/api/books/" + id;
+  //   await axios
+  //     .delete(url)
+  //     .then(() => {
+  //       window.location.reload();
+  //     })
+  //     .catch((error) => console.log("Error: ", error));
+  // }
 
-  async editBook(books, update) {
-    const id = user._id;
-    const url = "/api/books/" + id;
-    try {
-      await axios.put(url, { role: update });
-      window.location.reload();
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  }
+  // async editBook(books, update) {
+  //   const id = user._id;
+  //   const url = "/api/books/" + id;
+  //   try {
+  //     await axios.put(url, { role: update });
+  //     window.location.reload();
+  //   } catch (error) {
+  //     console.log("Error: ", error);
+  //   }
+  // }
 
-  async editBalance(user, newBalance) {
-    const id = user._id;
-    const url = "/api/users/" + id;
-    try {
-      await axios.put(url, { balance: newBalance });
-      // window.location.reload();
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  }
+  // async editBalance(user, newBalance) {
+  //   const id = user._id;
+  //   const url = "/api/books/" + id;
+  //   try {
+  //     await axios.put(url, { balance: newBalance });
+  //     // window.location.reload();
+  //   } catch (error) {
+  //     console.log("Error: ", error);
+  //   }
+  // }
 
   changePage = (e, p) => {
     this.setState((state) => ({
@@ -62,10 +63,10 @@ export default class Books extends React.Component {
     }));
   };
 
-  getPaginatedUsers(currentPage) {
+  getPaginatedBooks(currentPage) {
     const firstPageIndex = (currentPage-1) * this.state.pageSize;
     const lastPageIndex = firstPageIndex + this.state.pageSize;
-    return this.state.users.slice(firstPageIndex, lastPageIndex);
+    return this.state.books.slice(firstPageIndex, lastPageIndex);
   };
 
   render() {
@@ -103,11 +104,11 @@ export default class Books extends React.Component {
               ))} */}
             </tbody>
           </table>
-          <Pagination
-            count={Math.ceil(this.state.users.length / this.state.pageSize)}
+          {/* <Pagination
+            count={Math.ceil(this.state.books.length / this.state.pageSize)}
             page={this.state.currentPage}
             onChange={this.changePage}
-          />
+          /> */}
         </div>
       </div>
     ) : (
