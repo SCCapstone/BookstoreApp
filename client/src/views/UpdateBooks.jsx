ba94319a5cadf92fa4d565b26a128595a777fe61
s-orug <orugants@email.sc.edu>
2023-04-16 10:09:52 -0400

    restore#1

--- client/src/views/UpdateBooks.jsx
+++ client/src/views/UpdateBooks.jsx
@@ -61,6 +61,7 @@ export default class UpdateBooks extends React.Component {
     this.setState((state) => ({
       currentPage: p,
     }));
+    this.getBooks();
   };
 
   getPaginatedBooks(currentPage) {
