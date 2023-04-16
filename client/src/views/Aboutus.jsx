60960c3e4f5847103e1844b5b70da79d1f092108
Rahul Bulusu <rbulusu@email.sc.edu>
2023-04-16 11:00:01 -0400

    added the headshots of everyone in the assets folder

--- client/src/views/Aboutus.jsx
+++ client/src/views/Aboutus.jsx
@@ -9,14 +9,8 @@ const Aboutus = () => {
           About Us
         </div>
       </div>
-      <div class="xl:ml-20 xl:w-8/12 lg:w-8/12 md:w-4/12 w-4/12 py-2 grid grid-rows-auto max-w-[1400px]">
-        <p className={`${styles.paragraph} max-w-[1158px]`}>
-          The Bugsy's Barn Books was created by a group of University of South Carolina students to fulfill the 
-          requirements of CSCE 490 (Capstone Computing Project I) and CSCE 492 (Capstone Computing Project II). 
-          The members of this project are Rahul Bulusu, William Hobbs, Alfred Lin, Jack Oberman, and Sai Oruganti. 
-          {/* Add linkedin links of everyone as well as a headshot*/}
-        </p>
-        <p className={`${styles.paragraph} max-w-[1158px]`}>
+      <div>
+      <p>
           Now you might be wondering why we chose to create a website on a bookstore? 
           The vision of this came from our group member, Jack Oberman. Over the summer, he had worked 
           at a local bookstore called The Book Dispensary. He had brought the problem up of how bad their website
@@ -26,6 +20,12 @@ const Aboutus = () => {
           bookstore for users to experience. Our bookstore website, even though it has a been based on The Book Dispensary,
           has become so much more with many different unique features that users can experince.
           The platform could really revolutionize the making of websites for bookstores in the near future as well. 
+        </p>
+        <p>
+          The Bugsy's Barn Books was created by a group of University of South Carolina students to fulfill the 
+          requirements of CSCE 490 (Capstone Computing Project I) and CSCE 492 (Capstone Computing Project II). 
+          The members of this project are Rahul Bulusu, William Hobbs, Alfred Lin, Jack Oberman, and Sai Oruganti. 
+          {/* Add linkedin links of everyone as well as a headshot*/}
         </p>
       </div>
     </section>
