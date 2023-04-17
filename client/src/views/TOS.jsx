0faec026ae7489aef60420358a4c6618316d0356
Alfred Lin <alfredl@email.sc.edu>
2023-04-17 17:39:21 -0400

    changes

--- client/src/views/TOS.jsx
+++ client/src/views/TOS.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { styles } from "../styles";
 
 const longText =
-  "Our site uses an order form for customers to request information, products, and services. We request visitor's contact information (name, address, and email address) and financial information (payment information to make purchases). Contact information from the order form is used to ship orders to customers. The customer's contact information is used to contact the visitor when necessary. Financial information that is collected is used to bill the customer for products and services.";
+  "";
 const longText2 =
   "The website has security measures in place to protect the loss, misuse, and alteration of the information under our control. We use 128-bit SSL encryption to protect your financial information";
 const longText3 =
   "We provide users the following information options for removing their information from our database and to not receive any further communications from our service.";
 const longText4 =
   "If you have any questions about our terms and service, the practices of this site, or your dealings with our website, feel free to contact us at: ";
-const shortText = "The Book Dispensary";
-const shortText1 = "710-C Gracern Rd,";
+const shortText = "The Bugsy's Barn Books";
+const shortText1 = "123 UniversitySC Ave,";
 const shortText2 = "Columbia, SC 29210";
-const point1 = "You can send an email to info@mybookdispensary.com";
-const point2 = "You can send a letter to the following postal address.";
+const point1 = "";
+const point2 = "";
 
 const TOS = () => {
   return (
