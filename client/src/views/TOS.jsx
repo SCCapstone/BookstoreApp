f5c66c73e9a7a85ad3f112dc13e3de49abad509a
Alfred Lin <alfredl@email.sc.edu>
2023-04-17 17:59:49 -0400

    updated Term of Service

--- client/src/views/TOS.jsx
+++ client/src/views/TOS.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { styles } from "../styles";
 
 const longText =
-  "";
+  "This is a FAKE bookstore.";
 const longText2 =
-  "The website has security measures in place to protect the loss, misuse, and alteration of the information under our control. We use 128-bit SSL encryption to protect your financial information";
+  "We do not sell real books even though we have examples of books, those are for demostrations only.";
 const longText3 =
-  "We provide users the following information options for removing their information from our database and to not receive any further communications from our service.";
+  "Do not use real payments such as the following below:";
 const longText4 =
-  "If you have any questions about our terms and service, the practices of this site, or your dealings with our website, feel free to contact us at: ";
+  "You will not recieve any warranty, we do not isssue any warranty, NO EXCEPTIONS.";
+const longText5 =
+  "THIS WEBSITE IS ONLY FOR EDUCATION PURPOSE ONLY";
 const shortText = "The Bugsy's Barn Books";
 const shortText1 = "123 UniversitySC Ave,";
 const shortText2 = "Columbia, SC 29210";
-const point1 = "";
-const point2 = "";
+const point1 = "Credit Cards / Debit Cards / etc...";
+const point2 = "PayPal / AppplePay / GooglePay / etc...";
 
 const TOS = () => {
   return (
@@ -30,6 +32,7 @@ const TOS = () => {
         <li className={`${styles.paragraph} max-w-[1158px] ml-8`}>{point1}</li>
         <li className={`${styles.paragraph} max-w-[1158px] ml-8`}>{point2}</li>
         <p className={`${styles.paragraph} max-w-[1158px]`}>{longText4}</p>
+        <p className={`${styles.paragraph} max-w-[1158px]`}>{longText5}</p>
         <p className={`${styles.paragraph} max-w-[1158px]`}>{shortText}</p>
         <p className={`${styles.paragraph} max-w-[1158px]`}>{shortText1}</p>
         <p className={`${styles.paragraph} max-w-[1158px]`}>{shortText2}</p>
