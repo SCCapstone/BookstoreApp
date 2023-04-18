bdcd759db5fbaa17bf74b51bdc1e20a4e2ee1779
Rahul Bulusu <rbulusu@email.sc.edu>
2023-04-19 03:23:50 +0530

    added the main features of the app, vision, marketing statement, and added heading labels.

--- client/src/views/Aboutus.jsx
+++ client/src/views/Aboutus.jsx
@@ -5,6 +5,13 @@ import hobbs_headshot from "../assets/hobbs_headshot.jpg";
 import alfred_headshot from "../assets/alfred_headshot.png";
 import jack_headshot from "../assets/jack_headshot.jpg";
 import sai_headshot from "../assets/sai_headshot.jpg";
+import browse from "../assets/browse.jpg";
+import validatedUsers from "../assets/validatedUsers.jpg";
+import contactUs from "../assets/contactUs.jpg";
+import orders from "../assets/orders.jpg";
+import exampleBook from "../assets/exampleBook.jpg";
+import editBlog from "../assets/editBlog.jpg";
+import employeePage from "../assets/employeePage.jpg";
 
 const Aboutus = () => {
   return (
@@ -15,16 +22,29 @@ const Aboutus = () => {
         </div>
       </div>
       <div>
-      {/* Selling Advertisement statement for our bookstore*/}
+      {/* Selling Advertisement statement for why users should use our bookstore*/}
+      <h1 className="text-2xl font-medium mb-4 text-center">
+          Marketing Statement for Bugsy's Barn Books
+      </h1>
       <p>
-        For many customers, the first option is the selected choice simply out of convenience. It is generally guaranteed to be successful and reduces effort. 
-        While the second option does not require an intense effort, it shifts the effort on the book seller. Finally, the last option does not guarantee success and requires significant effort. 
-        A website that enables independent bookstores to upload, maintain, and sell their books would solve both the second and third issues, offering them a lifeline against the big-box retail booksellers.   
+        Our bookstore web application provides an effortless and seamless browsing experience, 
+        allowing users to easily discover and purchase a wide range of books from various genres, authors, 
+        and sort by various other options all in one convenient place.
+        With our user-friendly interface, our bookstore web application also provides convenient features such as book reviews, 
+        ratings (out of 5 stars), and adding a book to a wishlist, empowering users with 
+        valuable insights and information to make informed purchasing decisions, fostering a sense of trust and credibility in our platform. 
+        For admin/employees, our application gives a seamless experience for them to add blogs and special deals, add and update books, fulfill orders on time, 
+        and a place for employees to see important meetings/events. 
+        Lastly, our bookstore web application prioritizes user security and privacy with safely storing user's account information in the database that only
+        employee/admin are allowed to look at. All user's are required to have some form of a complex password to ensure this. 
       </p>
       {/* The vision of the bookstore */}
+      <h1 className="text-2xl font-medium mb-4 text-center">
+            Vision of Bugsy's Barn Books
+      </h1>
       <p> 
           The vision of this website came from our group member, Jack Oberman. Over the summer, he had worked 
-          at a local bookstore called The Book Dispensary. He had brought the problem up of how bad their website
+          at a local bookstore called The Book Dispensary. He had brought the problem up of how low quality their website
           is, with the color scheme, the attributes not working, and how it hasn't been maintained in a good bit.
           So, as a group we decided to essentially refurbish their website. Later into the second part of the 
           semester we had created our own logo for a bookstore, so we branched out and created our own custom, dummy 
@@ -32,15 +52,103 @@ const Aboutus = () => {
           has become so much more with many different unique features that users can experince.
           The platform could really revolutionize the making of websites for bookstores in the near future as well. 
         </p>
-        {/* Screenshots of the App */}
-
-        {/* short discription of how the app works and how to navigate */}
+        {/* Screenshots of the App and short discription of how the app works and how to navigate */}
+        <div>
+          <h1 className="text-2xl font-medium mb-4 text-center">
+            How does Bugsy's Barn Books work?
+          </h1>
+          <p>
+            In order for user's to use our site, first, they need to login to their registered account. If user's 
+            don't have an account, then they will need to register. Depending on the role of the user, they will have
+            more options on the side bar. 
+            If users are customers, then they can buy books and add reviews under the browse page.
+            They can navigate to shopping cart icon to purchase.  
+            If users are employees/admin, they can have the same features as customers plus they have the ability
+            to add and update books, add blogs and latest deals, and access the employee home page. 
+            If users have questions then they can contact us.
+          </p>
+          <div>
+            <img
+              src={browse} 
+              alt="Browse"
+              className="max-w-[5999px]"
+            />
+            <img
+              src={exampleBook} 
+              alt="Example Book"
+              className="max-w-[5999px]"
+            />
+            <p>
+              The Browse Page is where userca can view all the books that are available in our bookstore inventory for potential purchase. 
+              Users can find a book in multiple ways by simply browsing around, searcing using the book title, or by the 
+              “Sort by” dropdown menu feature which has the following options: “Best Selling,” “New Arrivals,” “Price: low to high,” or “Price: high to low.” 
+            </p>
+          </div>
+          <img 
+            src={orders} 
+            alt="Orders"
+            className="max-w-[5999px]"
+          />
+          <p>
+            The Orders page is restricted to employees/admin. This page is where employees/admin can see the customer’s orders and fulfill them. 
+            Specifically, it shows the order time which is formatted as the date followed by the exact time appended, the user's name which is their username, email address, exact order with the number of each type of book, total price, and status which is either shown as “In-Progress” or “Fulfilled.” 
+            Once the order has been fulfilled the employees/admin can delete the order to show that it is finished. Additionally, if the customer no longer wants that order fulfilled, they can contact the bookstore to delete the order as well.
+          </p>
+          <img 
+            src={validatedUsers} 
+            alt="Validated Users"
+            className="max-w-[5999px]"
+          />
+          <p>
+            The Validated Users page is restricted to employees/admin, and it shows all the users who have accounts in the database. 
+            Employees/admin have the ability to promote a customer to admin if they are employees of course. 
+            Additionally, this is where admin can update/hardcode the balance for purchasing books. 
+            The validated users page is broken into 5 categories: name of the user, email of the user, action (means the user can be deleted from our database upon request), the role (either customer or admin – resembles either employee or admin), and the balance for purchasing books.   
+          </p>
+          <img 
+            src={contactUs} 
+            alt="Contact Us"
+            className="max-w-[5999px]"
+          />
+          <p>
+            The Contact Us page is for if users have questions about anything relating to the bookstore website or purchasing books.
+            Users can get in touch one of two ways: by email by filling out a small form which consists of the user’s name, email, and the message 
+            or by calling us directly using the provided number on the website under the “Phone” heading.
+          </p>
+          <img 
+            src={employeePage} 
+            alt="Employee Home Page"
+            className="max-w-[5999px]"
+          />
+          <p>
+            The Employee Page is restricted to employees/admin and is to be used as an employee/admin home page. 
+            This is where employees can view and create meetings for important messages and updates pertaining to the bookstore. 
+            A calendar is available showing meetings and events taking place during that month.
+            Employees have the ability to delete any event/meeting as well from the calendar. 
+            Adding an event to the calendar is very simple: employees need to add the title of the event, and the starting and ending times. 
+            They also have the ability to access the validated users page by clicking on “Click here to see user.” 
+            They also can get redirected to the “Edit Blog Page” by clicking “Click here to edit blog post.” 
+            Finally, they can get redirected to the orders page where they can view and fulfill/delete book orders by clicking on “Click here to order books.”  
+          </p>
+          <img 
+            src={editBlog} 
+            alt="Edit Blog"
+            className="max-w-[5999px]"
+          />
+          <p>
+            The Edit Blog page which is restricted to employees/admin. 
+            This can serve as both a blogs page as well as new information and latest deals. 
+            Once an employee/admin creates and posts a blog, it appears on this page as well as the “Home” page where all users can view as well. 
+            It is a very simple process: employees need to enter the title of the blog in the first box and enter a brief description in the next box. Then they can click post. 
+            They also have the ability to delete a blog as well. 
+          </p>
+        </div>
 
         {/* short description about the group members */}
         <p>
-          The Bugsy's Barn Books was created by a group of University of South Carolina students to fulfill the 
-          requirements of CSCE 490 (Capstone Computing Project I) and CSCE 492 (Capstone Computing Project II). 
-          The members of this project are Rahul Bulusu, William Hobbs, Alfred Lin, Jack Oberman, and Sai Oruganti.
+          The Bugsy's Barn Books was created by a group of University of South Carolina Computer Science and Computer Engineering
+          students to fulfill the requirements of CSCE 490 (Capstone Computing Project I) and CSCE 492 
+          (Capstone Computing Project II). The members of this project are Rahul Bulusu, William Hobbs, Alfred Lin, Jack Oberman, and Sai Oruganti.
           All work done for this website was split fairly among the group members.  
         </p>
         {/* Add linkedin and github links of everyone as well as a headshot*/}
