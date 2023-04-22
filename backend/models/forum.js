799694b025fa01f159393571ec8ccc33ed1114b7
Rahul Bulusu <rbulusu@email.sc.edu>
2023-04-22 02:28:26 -0400

    added comments to forum.js page

--- backend/models/forum.js
+++ backend/models/forum.js
@@ -1,12 +1,16 @@
+//this is the backend for the forum page which utilizes MongoDB to store all the forums
+//initialize mongoose and Joi
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
+//the reply function uses the replyingUserID which is a String, a post which is a String, and a date which is a date format
 const reply = {
     replyingUserID: String,
     post: String,
     date: Date,
 }
 
+//forum schema which stores the schema in mongoose which has a post, date, uuid, (all type string and true) and replies which is of type reply and set to false
 const forumSchema = new mongoose.Schema({
     post: { type: String, required: true },
     date: { type: String, required: true },
@@ -15,9 +19,10 @@ const forumSchema = new mongoose.Schema({
 });
 
 // replies will be a json object with name and reply so people can reply
-
 const Forum = mongoose.model("forums", forumSchema);
 
+//validate forums and put into the data using the User ID, post, and the date of when it was posted 
+//@param data used to store in monoDB
 const validate = (data) => {
     const schema = Joi.object({
         uuid: Joi.string().required().label("User ID"),
